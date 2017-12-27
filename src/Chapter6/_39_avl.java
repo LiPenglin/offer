984ad6d703336883e1010c54975927d4c1e67129
LiPenglin <529884069@qq.com>
2017-12-27 16:52:12 +0800

    39 二叉树的深度

---
+++ src/Chapter6/_39_avl.java
@@ -0,0 +1,42 @@
+package Chapter6;
+
+public class _39_avl {
+	public static void main(String[] args) {
+		TreeNodeI root = new TreeNodeI(1);
+		root.left = new TreeNodeI(2);
+		root.right = new TreeNodeI(2);
+		root.left.left = new TreeNodeI(3);
+		root.left.left.left = new TreeNodeI(4);
+		System.out.println(new _39_functionI().is_avl(root));
+	}
+}
+
+class _39_functionI {
+	int max = 0x7fffffff;
+	// ×óÓÒ×ÓÊ÷ Éî¶È²î <= 1
+	boolean is_avl(TreeNodeI root) {
+		return is_balanced(root) == max ? false : true;
+	}
+	int is_balanced(TreeNodeI root) {
+		if (root == null) return 0;
+		int left;
+		int right;
+		if ((left = is_balanced(root.left)) != max
+				&& (right = is_balanced(root.right)) != max) {
+				int diff = left - right;
+				if (diff <= 1 && diff >= -1) return  1 + (left > right ? left : right);
+			}
+		return max;
+	}
+}
+
+class TreeNodeI {
+	int data;
+	TreeNodeI left;
+	TreeNodeI right;
+	public TreeNodeI(int data) {
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+}

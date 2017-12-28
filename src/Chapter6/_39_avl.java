499a356a00e70f14e0c685d591d7dc0b7e5de7df
LiPenglin <529884069@qq.com>
2017-12-28 11:02:41 +0800

    39 二叉树的深度

--- src/Chapter6/_39_avl.java
+++ src/Chapter6/_39_avl.java
@@ -1,5 +1,10 @@
 package Chapter6;
 
+/**
+ * 面试题39：二叉树的深度
+ * 输入一棵二叉树，求该树的深度。从根结点到叶结点依次经过的结点（含根、叶结点）形成树的一条路径，最长路径的长度为树的深度。
+ */
+
 public class _39_avl {
 	public static void main(String[] args) {
 		TreeNodeI root = new TreeNodeI(1);
